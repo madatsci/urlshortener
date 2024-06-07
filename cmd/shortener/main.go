3e4b6e25587225751988551c221af1470dbb0e69
enguipro <sedemaqu@gmail.com>
2024-06-07 22:19:26 +0300

    Basic server implementation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,70 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const slugLength = 8
+
+type apiHandler struct {
+	urls map[string]string
+}
+
+func (a *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		url := string(body)
+
+		slug := generateRandomString(slugLength)
+		shortURL := "http://localhost:8080/" + slug
+
+		a.urls[slug] = url
+
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(shortURL))
+	case http.MethodGet:
+		shortURL := strings.Trim(r.URL.Path, "/")
+
+		url, ok := a.urls[shortURL]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	var handler apiHandler
+	handler.urls = make(map[string]string)
+	mux.Handle(`/`, &handler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func generateRandomString(length int) string {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/madatsci/urlshortener
+
+go 1.22.3
