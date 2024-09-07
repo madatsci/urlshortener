c28c04ded647dea6f760d719124e4aa4232d3997
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-09-07 10:22:15 +0300

    Added tests for storages

---
+++ internal/app/storage/filestorage/storage_test.go
@@ -0,0 +1,52 @@
+package filestorage
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorageWithEmptyFile(t *testing.T) {
+	type urlData struct {
+		slug string
+		url  string
+	}
+
+	urls := []urlData{
+		{
+			slug: "rQujOeua",
+			url:  "https://practicum.yandex.ru/",
+		},
+		{
+			slug: "jViVdkfU",
+			url:  "http://example.org",
+		},
+	}
+
+	filepath := "./test_storage.txt"
+	s, err := New(filepath)
+	require.NoError(t, err)
+	defer func() {
+		err := os.Remove(filepath)
+		require.NoError(t, err)
+	}()
+
+	all := s.ListAll()
+	assert.Equal(t, 0, len(all))
+
+	for _, d := range urls {
+		err := s.Add(d.slug, d.url)
+		require.NoError(t, err)
+	}
+
+	for _, d := range urls {
+		url, err := s.Get(d.slug)
+		require.NoError(t, err)
+		assert.Equal(t, d.url, url)
+	}
+
+	all = s.ListAll()
+	require.Equal(t, 2, len(all))
+}
---
+++ internal/app/storage/memstorage/storage_test.go
@@ -0,0 +1,42 @@
+package memstorage
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorage(t *testing.T) {
+	type urlData struct {
+		slug string
+		url  string
+	}
+
+	urls := []urlData{
+		{
+			slug: "rQujOeua",
+			url:  "https://practicum.yandex.ru/",
+		},
+		{
+			slug: "jViVdkfU",
+			url:  "http://example.org",
+		},
+	}
+
+	s, err := New()
+	require.NoError(t, err)
+
+	for _, d := range urls {
+		s.Add(d.slug, d.url)
+	}
+
+	for _, d := range urls {
+		url, err := s.Get(d.slug)
+		require.NoError(t, err)
+		assert.Equal(t, d.url, url)
+	}
+
+	all := s.ListAll()
+	require.Equal(t, 2, len(all))
+}
