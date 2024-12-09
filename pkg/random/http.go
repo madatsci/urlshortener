070c19c8289b1cba5c40a2ece873a9eacd209861
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-12-09 22:30:57 +0300

    Added URL generation to pkg/random

---
+++ pkg/random/http.go
@@ -0,0 +1,32 @@
+package random
+
+import (
+	"math/rand"
+	"net/url"
+	"strings"
+	"time"
+)
+
+var zones = []string{"com", "ru", "net", "biz", "org"}
+
+func URL() *url.URL {
+	var res url.URL
+
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	res.Scheme = "http"
+	res.Host = domain(5, 15)
+
+	for i := 0; i < rand.Intn(4); i++ {
+		res.Path += "/" + strings.ToLower(ASCIIStringVarLength(5, 15))
+	}
+	return &res
+}
+
+func domain(minLen, maxLen int) string {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	zone := zones[rand.Intn(len(zones))]
+	host := strings.ToLower(ASCIIStringVarLength(minLen, maxLen))
+
+	return host + "." + zone
+}
