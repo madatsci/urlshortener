07c124efa9a8d1891d4f2fc0c42a824448f46a6d
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-12-09 22:30:02 +0300

    Added URL generation to random pkg

--- pkg/random/slug.go
+++ pkg/random/slug.go
@@ -9,9 +9,22 @@ const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func ASCIIString(length int) string {
 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+	s := make([]byte, 0, length)
+	i := 0
+	for len(s) < length {
+		idx := rand.Intn(len(charset))
+		char := charset[idx]
+		if i == 0 && '0' <= char && char <= '9' {
+			continue
+		}
+		s = append(s, char)
+		i++
 	}
-	return string(b)
+	return string(s)
+}
+
+func ASCIIStringVarLength(minLen, maxLen int) string {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	slen := rand.Intn(maxLen-minLen) + minLen
+	return ASCIIString(slen)
 }
